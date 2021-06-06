b24c5f2c79338242581e68a56aa4e6cbd7780ec6
luis.moneda <lg.moneda@gmail.com>
2021-06-06 09:04:03 -0300

    package template

---
+++ time_robust_forest/__init__.py
@@ -0,0 +1,13 @@
+# type: ignore[attr-defined]
+"""Explores time information to train a robust random forest"""
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
